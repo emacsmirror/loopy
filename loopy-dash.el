b0c27ea9b84c05e49d1464d18f89bcbf59eb5090
okamsn <okamsn@users.noreply.github.com>
2021-02-14 00:32:40 -0500

    Add a Dash-based accumulation parser.
    Uses Dash destructuring for accumulations.

--- loopy-dash.el
+++ loopy-dash.el
@@ -43,6 +43,17 @@
 (defvar loopy--destructuring-function)
 (defvar loopy--flags-setup)
 
+(defun loopy--flag-setup-dash ()
+  "Make this `loopy' loop use Dash destructuring."
+  (setq
+   loopy--destructuring-function #'loopy--create-destructured-assignment-dash
+   loopy--accumulation-parser #'loopy--parse-accumulation-commands-dash))
+
+(with-eval-after-load 'loopy
+  (add-to-list 'loopy--flags-setup
+               (cons 'dash #'loopy--flag-setup-dash)))
+
+;;;; The actual functions:
 (defun loopy--create-destructured-assignment-dash
     (var value-expression)
   "Create a list of instructions for using Dash's destructuring in `loopy'."
@@ -51,15 +62,50 @@
                destructurings)
       (loopy--main-body . (setq ,@(-flatten-n 1 destructurings))))))
 
+(defun loopy--dash-vars-to-list (sequence)
+  "Return a list (maybe of sublists) of elements in sequence.
 
-(defun loopy--flag-setup-dash ()
-  "Make this `loopy' loop use Dash destructuring."
-  (setq loopy--destructuring-function
-        #'loopy--create-destructured-assignment-dash))
+Needed since `-flatten' doesn't work on vectors."
+  (cond
+   ((symbolp sequence) sequence)
+   ((consp sequence) sequence)
+   (t
+    (seq-map #'loopy--dash-get-vars sequence))))
 
-(with-eval-after-load 'loopy
-  (add-to-list 'loopy--flags-setup
-               (cons 'dash #'loopy--flag-setup-dash)))
+(cl-defun loopy--parse-accumulation-commands-dash ((name var val))
+  "Parse the accumulation loop commands, like `collect', `append', etc.
+
+NAME is the name of the command.  VAR-OR-VAL is a variable name
+or, if using implicit variables, a value .  VAL is a value, and
+should only be used if VAR-OR-VAL is a variable."
+  (let ((var-list (-flatten (loopy--dash-vars-to-list var))))
+    `(,@(-mapcat
+         (-lambda ((sub-var sub-val))
+           ;; Only apply accumulation to named sub-variables.  If symbol is
+           ;; a Dash source, don't accumulate, just let through.
+           (if (memq sub-var var-list)
+               `((loopy--explicit-vars
+                  . (,sub-var ,(cl-case name
+                                 ((sum count)    0)
+                                 ((max maximize) -1.0e+INF)
+                                 ((min minimize) +1.0e+INF))))
+                 (loopy--main-body
+                  . ,(cl-ecase name
+                       (append `(setq ,sub-var (append ,sub-var ,sub-val)))
+                       (collect `(setq ,sub-var (append ,sub-var (list ,sub-val))))
+                       (concat `(setq ,sub-var (concat ,sub-var ,sub-val)))
+                       (vconcat `(setq ,sub-var (vconcat ,sub-var ,sub-val)))
+                       (count `(if ,sub-val (setq ,sub-var (1+ ,sub-var))))
+                       ((max maximize) `(setq ,sub-var (max ,sub-var ,sub-val)))
+                       ((min minimize) `(setq ,sub-var (min ,sub-var ,sub-val)))
+                       (nconc `(setq ,sub-var (nconc ,sub-var ,sub-val)))
+                       ((push-into push) `(push ,sub-val ,sub-var))
+                       (sum `(setq ,sub-var (+ ,sub-var ,sub-val)))))
+                 (loopy--implicit-return . ,sub-var))
+             `((loopy--implicit-vars . (,sub-var nil))
+               (loopy--main-body . (setq ,sub-var ,sub-val)))))
+         ;; Get Dash's destructuring, and feed to the lambda.
+         (dash--match var val)))))
 
 (provide 'loopy-dash)
 ;;; loopy-dash.el ends here
